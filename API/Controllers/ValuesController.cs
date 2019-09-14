3d4ddfba49ac21e994751507e2b5a2c4f307f80f
Ramzan Khan <Ramzan@RELYON.COM>
2019-09-15 00:44:59 +0530

    initail commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dbcontext;
+        public ValuesController(DataContext dbcontext)
+        {
+            this._dbcontext = dbcontext;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<Value>> Get()
+        {
+            var values = await _dbcontext.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _dbcontext.Values.FindAsync(id);
+            return  Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+             using (var scope = host.Services.CreateScope())
+             {
+                 var servies = scope.ServiceProvider;
+                 try
+                 {
+                   var context  = servies.GetRequiredService<DataContext>();  
+                       context.Database.Migrate(); 
+                 }
+                 catch(Exception ex) 
+                 {
+                     var logger = servies.GetRequiredService<ILogger<Program>>();
+                     logger.LogError(ex, "An Error occured during migration");
+                 }
+             }   
+             host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
